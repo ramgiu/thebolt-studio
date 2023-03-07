19deff126eb5636f0cd7fc69c980054839bc581d
ramgiu <ramiro.giunta@gmail.com>
2023-03-06 23:25:04 -0300

    cambiando email

--- enviar-2.php
+++ enviar-2.php
@@ -8,11 +8,11 @@ $salto = "\r\n";
 $destino1 = $email;
 $asunto1 = "Respuesta a su consulta vía web";
 $mensaje1 = "Estimado ".$nombre." esta es una respuesta automática a la consulta que ud. realizara en nuestra página web, a la brevedad será analizada y nos pondremos en contacto.";
-$remite1 = "From: The Bolt <rami_200_2@hotmail.com>";
+$remite1 = "From: The Bolt <hello@thebolt-studio.com>";
 
 mail($destino1, $asunto1, $mensaje1, $remite1);
 
-$destino2 = "rami_200_2@hotmail.com";
+$destino2 = "hello@thebolt-studio.com";
 $asunto2 = $nombre.", te ha enviado un mensaje.";
 $mensaje2 = "Nombre: ".$nombre.$salto."Email: ".$email.$salto."Mensaje: ".$mensaje;
 $remite2 = "From: $nombre <$email>";
